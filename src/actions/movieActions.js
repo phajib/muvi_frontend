fd27921d719218659e20571063298c5b1e5ac52a
Haj B <phajib@gmail.com>
2020-03-28 10:52:37 -0700

    user and movie actions set

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,48 +1,41 @@
-import { SEARCH_MOVIE, FETCH_MOVIES,
-  FETCH_MOVIE, LOADING,
-  FETCH_UPCOMING, FETCH_TOP_RATED,
-  FETCH_POPULAR, FETCH_LATEST
-} from './types';
 import axios from 'axios';
-
-// import { APIKey } from '../APIKey';
 
 let HOST_URL = "http://localhost:3001/api/v1"
 
 export const setLoading = () => {
   return {
-    type: LOADING
+    type: 'LOADING'
   };
 };
 
 //-----------MOVIES--------------------
 export const searchMovie = text => dispatch => {
   dispatch({
-    type: SEARCH_MOVIE,
+    type: 'SEARCH_MOVIE',
     payload: text
   });
 };
 
 export const fetchMovies = text => dispatch => {
   axios
-    .get(`${HOST_URL}/search/${text}`)
-    // .get(`https://www.omdbapi.com/?apikey=${APIKey}&s=${text}`)
+    .get(`https://api.themoviedb.org/3/search/movie?api_key=2c7ac09213443b720c7abaa7b2c0ec29&query=${text}`)
+    // .get(`${HOST_URL}/search/${text}`)
     .then(response =>
       dispatch({
-        type: FETCH_MOVIES,
-        payload: response.data
+        type: 'FETCH_MOVIES',
+        payload: response.data,
       })
     )
     .catch(err => console.log(err));
 };
 
-export const fetchMovie = id => dispatch => {
+export const fetchMovie = movie_id => dispatch => {
   axios
-    // .get(`https://www.omdbapi.com/?apikey=${APIKey}&i=${id}`)
-    .get(`${HOST_URL}/movie/${id}`)
+    .get(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=2c7ac09213443b720c7abaa7b2c0ec29`)
+    // .get(`${HOST_URL}/movie/${movie_id}`)
     .then(response =>
       dispatch({
-        type: FETCH_MOVIE,
+        type: 'FETCH_MOVIE',
         payload: response.data
       })
     )
@@ -51,10 +44,11 @@ export const fetchMovie = id => dispatch => {
 
 export const fetchUpcoming = dispatch => {
   axios
+  // .get(`https://api.themoviedb.org/3/movie/upcoming?api_key=2c7ac09213443b720c7abaa7b2c0ec29`)
     .get(`${HOST_URL}/upcoming`)
     .then(response =>
       dispatch({
-        type: FETCH_UPCOMING,
+        type: 'FETCH_UPCOMING',
         payload: response.data
       })
     )
@@ -63,22 +57,25 @@ export const fetchUpcoming = dispatch => {
 
 export const fetchTopRated = dispatch => {
   axios
-    .get(`${HOST_URL}/top_rated`)
+  .get(`https://api.themoviedb.org/3/movie/top_rated?api_key=2c7ac09213443b720c7abaa7b2c0ec29`)
+    // .get(`${HOST_URL}/top_rated`)
     .then(response =>
       dispatch({
-        type: FETCH_TOP_RATED,
+        type: 'FETCH_TOP_RATED',
         payload: response.data
       })
-    )
-    .catch(err => console.log(err));
-};
-
-export const fetchPopular = dispatch => {
-  axios
-    .get(`${HOST_URL}/popular/1`)
+      )
+      .catch(err => console.log(err));
+    };
+    
+    export const fetchPopular = dispatch => {
+      console.log("Gonna try and fetch popular movies")
+      axios
+      // .get(`https://api.themoviedb.org/3/movie/popular?api_key=2c7ac09213443b720c7abaa7b2c0ec29&language=en-US&page=1`)
+      .get(`${HOST_URL}/popular/1`)
     .then(response =>
       dispatch({
-        type: FETCH_POPULAR,
+        type: 'FETCH_POPULAR',
         payload: response.data
       })
     )
@@ -87,10 +84,11 @@ export const fetchPopular = dispatch => {
 
 export const fetchLatest = dispatch => {
   axios
-    .get(`${HOST_URL}/upcoming`)
+  // .get(`https://api.themoviedb.org/3/movie/latest?api_key=2c7ac09213443b720c7abaa7b2c0ec29`)
+    .get(`${HOST_URL}/latest`)
     .then(response =>
       dispatch({
-        type: FETCH_LATEST,
+        type: 'FETCH_LATEST',
         payload: response.data
       })
     )
--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,8 +1,89 @@
-import { 
-  } from './types';
-  import axios from 'axios';
+import axios from 'axios';
+import Swal from 'sweetalert2'
 
-  let HOST_URL = "http://localhost:3001/api/v1"
+let HOST_URL = "http://localhost:3001/api/v1"
 
-  //-----------USERS--------------------
-  
+// const CREATE_USER = 'CREATE_USER'
+// const CREATE_USER_ERROR = 'CREATE_USER_ERROR'
+// const CURRENT_USER = 'CURRENT_USER'
+
+//-----------USERS--------------------
+export const signUp = (userInfo) => {
+  return (dispatch) => {
+    axios.get(`${HOST_URL}/api/v1/users`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: userInfo.username,
+          password: userInfo.password,
+          about: userInfo.about,
+          profile_picture: userInfo.avatar,
+        }
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: 'CURRENT_USER', payload: data.user })
+        localStorage.setItem("jwt", data.jwt)
+      })
+  }
+}
+
+// export const signUp = newUser => async (dispatch) => {
+//   try {
+//     dispatch({ type: 'CREATE_USER', ...newUser});
+
+//     const response = await axios({
+//       method: 'POST',
+//       url: `${HOST_URL}/users`,
+//       data: { user: newUser },
+//       crossdomain: true
+//     });
+//     const { token } = response.data
+//     localStorage.setItem('jwt', token);
+//   } catch {
+//     dispatch({ type: 'CREATE_USER_ERROR' });
+//   }
+// };
+
+export const logIn = (userInfo) => {
+  return (dispatch) => {
+    axios.get(`${HOST_URL}/api/v1/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: userInfo.username,
+          password: userInfo.password
+        }
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.message === "INVALID: username or password") {
+          Swal.fire({
+            title: 'Unable to Login!',
+            text: `${data.message}`,
+            icon: 'error',
+            confirmButtonText: 'Back'
+          })
+        } else {
+          dispatch({ type: 'CURRENT_USER', payload: data.user })
+          localStorage.setItem("jwt", data.jwt)
+        }
+      })
+  }
+}
+
+export const signOut = () => {
+  localStorage.removeItem('jwt')
+  return {type: "SIGN_OUT", payload: []}
+}
+
