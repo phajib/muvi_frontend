e2ebba2d3268cf98a6ff37db967c26261355006f
Haj B <phajib@gmail.com>
2020-03-29 00:42:43 -0700

    added Popular, Top rated, About

--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import About from '../About/About'
+import { connect } from 'react-redux'
+// import PopularMovies from '../Home/PopularMovies';
+// import TopRatedMovies from '../Home/TopRatedMovies';
+// import About from '../About/About'
+import { signOut } from './../../actions/usersActions'
 
 function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-light bg-dark mb-5">
         <div className="container">
-          <div className="navbar-header">
+          <div className="navbar-header animated jackInTheBox">
             <Link className="navbar-brand text-white text-lg brand-text" to="/">
               MUVI
             </Link>
           </div>
           <ul className="navbar-nav ml-auto text-light d-inline-block">
-            <li className="nav-item d-inline-block mr-4">
-              <Link className="navbar-brand text-white text-lg brand-text" to="/about">
+            <li className="nav-item d-inline-block mr-4 animated jackInTheBox">
+              <Link className="navbar-brand text-white text-lg" to="/movies">
+                Movies
+              </Link>
+              <Link className="navbar-brand text-white text-lg" to="/about">
                 About
+              </Link>
+              <Link className="navbar-brand text-white text-sm" to="/popular/1">
+                Popular Movies
+              </Link>
+              <Link className="navbar-brand text-white text-sm" to="/top_rated">
+                Top Rated Movies
               </Link>
             </li>
             <li className="nav-item d-inline-block mr-4">
-              <i className="fab fa-imdb fa-2x" id="imdb-logo" />
-            </li>
-            <li className="nav-item d-inline-block mr-4">
-              <i className="fab fa-react fa-2x" id="react-logo" />
+              {!Array.isArray(this.props.currentUser) ? (
+                    <Link className="nav-item" to="/profile">
+                      {this.props.currentUser.username}'s Profile
+                    </Link>
+                ) : null}
+
+              {!Array.isArray(this.props.currentUser) ? (
+                // eslint-disable-next-line
+                <a onClick={() => this.props.signOut()} className="nav-item">
+                  Log Out
+                </a>
+              ) : (
+                  <Link className="nav-item" to="/login">Login</Link>
+              )}
             </li>
           </ul>
         </div>
@@ -32,4 +54,12 @@ function Navbar() {
   );
 }
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+  currentUser: state.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  signOut: () => dispatch(signOut())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
