5257a2db0485c28b098b959e9f93dba2acddd59a
Haj B <phajib@gmail.com>
2020-03-29 00:37:09 -0700

    cleaned up About

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,10 +4,15 @@ import { Link } from 'react-router-dom'
 function About() {
     return (
         <div className="about-page-container">
-                <div className="about-page-card">
+                <div className="about-page-card align-middle">
                     <div className="api-container">
                         <div className="api-image">
-                             <img className="logo-pic" alt="the movie db logo" src="https://www.themoviedb.org/assets/2/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png"></img>
+                            <img
+                                className="logo-pic"
+                                alt="the movie db logo"
+                                src="https://www.themoviedb.org/assets/2/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png"
+                                width="240" height = "100"
+                            />
                         </div>
 
                         <div className="api-text">
@@ -17,9 +22,22 @@ function About() {
 
                     <div className="built-container">
                         <div className ="build-img-container">
-                            <img alt="ruby on rails logo" src="https://d8bwfgv5erevs.cloudfront.net/cdn/13/images/curso-online-ruby-on-rails_l_primaria_1_1520261687.jpg"></img>
-                            <img alt="react logo" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"></img>
-                            <img alt="react redux" src="https://img.stackshare.io/service/7374/react-redux.png"></img>
+                            <img
+                                alt="ruby on rails logo" 
+                                src="https://d8bwfgv5erevs.cloudfront.net/cdn/13/images/curso-online-ruby-on-rails_l_primaria_1_1520261687.jpg"
+                                width="200" height = "100"
+                             />
+
+                            <img
+                                alt="react logo"
+                                src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+                                width="200" height = "100"
+                            />
+                            <img
+                                alt="react redux"
+                                src="https://img.stackshare.io/service/7374/react-redux.png"
+                                width="200" height = "100"
+                            />
                         </div>
 
                         <div className="buld-text-container">
