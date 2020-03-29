6334c905ae05dd5cf21de7619e8a56aa1dafec47
Haj B <phajib@gmail.com>
2020-03-29 00:38:00 -0700

    added background

--- src/Components/Home/Movie.js
+++ src/Components/Home/Movie.js
@@ -12,51 +12,58 @@ export class Movie extends Component {
     this.props.setLoading();
   }
   render() {
-    const { loading, movie } = this.props;
+    const { loading, movie } = this.props; //destructuring
+    let movieBackdrop = `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
 
     let movieInfo = (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4 card card-body">
-            <img src={movie.poster_path} className="thumbnail" alt="Poster" />
-          </div>
-          <div className="col-md-8">
-            <h2 className="mb-4">{movie.title}</h2>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <strong>Overview:</strong> {movie.overview}
-              </li>
-              <li className="list-group-item">
-                <strong>Genre:</strong> {movie.genre}
-              </li>
-              <li className="list-group-item">
-                <strong>Released:</strong> {movie.realease_date}
-              </li>
-              <li className="list-group-item">
-                <strong>Average Rating:</strong> {movie.vote_average}
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="row">
-          <div className="card card-body bg-dark my-5 text-light">
-            <div className="col-md-12">
-              <h3>About </h3>
-              {movie.Plot}
-              <hr />
-              <a
-                href={'https://www.imdb.com/title/' + movie.imdbID}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-primary"
-              >
-                View on IMDB
-              </a>
-              <Link to="/" className="btn btn-default text-light">
-                Go Back To Search
-              </Link>
+      <div style={{backgroundImage: `url(${movieBackdrop})`}}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 card card-body">
+              <img src={movie.poster_path} className="thumbnail" alt="Poster" />
+            </div>
+            <div className="col-md-8">
+              <h2 className="mb-4">{movie.title}</h2>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <strong>Overview:</strong> {movie.overview}
+                </li>
+                <li className="list-group-item">
+                  <strong>Genre:</strong> {movie.genre}
+                </li>
+                <li className="list-group-item">
+                  <strong>Released:</strong> {movie.realease_date}
+                </li>
+                <li className="list-group-item">
+                  <strong>Average Rating:</strong> {movie.vote_average}
+                </li>
+              </ul>
             </div>
           </div>
+          <div className="row">
+            <div className="card card-body bg-dark my-5 text-light">
+              <div className="col-md-12">
+                <h3>About</h3>
+                {movie.overview}
+                <hr />
+                <a
+                  href={'https://www.imdb.com/title/' + movie.imdbID}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-primary"
+                >
+                  View on IMDB
+                </a>
+
+                <Link to="/" className="btn btn-default text-light">
+                  Go Back To Search
+                </Link>
+              </div>
+            </div>
+          </div>
+          <button onClick={() => {props.addToList(props.movie)} } to='/movies'>
+            Save Movie
+          </button>
         </div>
       </div>
     );
