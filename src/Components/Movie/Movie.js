7da6b4821cd7770b4a32e1b20cea408ca56405a0
Haj B <phajib@gmail.com>
2020-04-03 09:49:35 -0700

    cleaned up comments and adjusted layout

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -3,11 +3,11 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { addToList } from '../../actions/movieActions'
 
-// import CommentForm from '../Comments/CommentForm'
+import CommentForm from '../Comments/CommentForm'
 // import MovieComments from '../Movie/MovieComments'
 
 import { fetchMovie, setLoading } from '../../actions/movieActions'
-
+// import Swal from 'sweetalert2'
 import Spinner from '../layout/Spinner'
 // debugger
 export class Movie extends Component {
@@ -15,6 +15,7 @@ export class Movie extends Component {
     this.props.fetchMovie(this.props.match.params.id);
     this.props.setLoading();
   }
+
   render() {
     const { loading, movie } = this.props; //destructuring
     const { genres, } = this.props.movie
@@ -25,54 +26,47 @@ export class Movie extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4 card card-body">
-              <img src={"https://image.tmdb.org/t/p/w300/" + movie.poster_path} className="thumbnail" alt="Poster" />
+              <img src={"https://image.tmdb.org/t/p/w342/" + movie.poster_path} className="thumbnail" alt="Poster" />
             </div>
             <div className="col-md-8">
-              <h2 className="mb-4 text-success">{movie.title}</h2>
               <ul className="list-group">
-                {/* <li className="list-group-item">
+                <li className="list-group-item bg-dark text-white">
+                  <h2 className="mb-4 text-white">{movie.title}</h2>
                   <strong>Overview:</strong> {movie.overview}
-                </li> */}
-                <li className="list-group-item">
-                  {genres && <p className="details"><strong>Genres</strong><br></br><span className="detailSpan">{genres.map(gen => gen.name).join(", ")} </span></p>}
+                  <h3 className="text-white">{movie.tagline}</h3>
+                  <div className="container">
+                    <button onClick={() => {addToList(movie, this.props.users)} } to='/movies' className="btn btn-success btn-sm">
+                      Save Movie
+                    </button>
+                  </div>
                 </li>
-                <li className="list-group-item">
-                  <strong>Released:</strong> {movie.release_date}
+                <li className="list-group-item bg-dark text-white">
+                  {genres && <p className="details">
+                    <strong>Genres</strong>
+                    <br></br>
+                    <span className="detailSpan">{genres.map(gen => gen.name).join(", ")} </span></p>}
+                  <strong>Released:</strong> {movie.release_date} <br></br>
+                  <strong>Average Rating:</strong> {movie.vote_average} <br></br>
                 </li>
-                <li className="list-group-item">
-                  <strong>Average Rating:</strong> {movie.vote_average}
+                <li className="list-group-item bg-dark text-white">  
+                  <a
+                    href={'https://www.imdb.com/title/' + movie.imdb_id}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-success"
+                  >
+                    View on IMDB
+                  </a>
+                  <Link to="/" className="btn btn-default text-success">
+                    Go Back To Search
+                  </Link>
+                </li>
+                <li>
+                  <CommentForm movie={this.props.movie} newCommentAdded={this.props.newCommentAdded} addComment={this.props.addComment} />
+                  {/* <MovieComments movieComments={this.props.movieComments} deleteComment={this.props.deleteComment} classes="" mycomments={false} user={this.props.user}/> */}
                 </li>
               </ul>
             </div>
-          </div>
-          <div className="row">
-            <div className="card card-body bg-dark my-5 text-light">
-              <div className="col-md-12">
-                <h3 className="text-success">Overview</h3>
-                {movie.overview}
-                <hr />
-                <a
-                  href={'https://www.imdb.com/title/' + movie.imdb_id}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-outline-success"
-                >
-                  View on IMDB
-                </a>
-
-                <Link to="/" className="btn btn-default text-success">
-                  Go Back To Search
-                </Link>
-              </div>
-            </div>
-          </div>
-          <button onClick={() => {addToList(movie)} } to='/movies' className="btn btn-success">
-            Save Movie
-          </button>
-          
-          <div className="comments five wide column">
-            {/* <CommentForm movie={this.props.movie} newCommentAdded={this.props.newCommentAdded} addComment={this.props.addComment} /> 
-            <MovieComments movieComments={this.props.movieComments} deleteComment={this.props.deleteComment} classes="" mycomments={false} user={this.props.user}/> */}
           </div>
         </div>
       </div>
