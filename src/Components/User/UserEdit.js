1cf9fa789393a77460ed1dcbcbb3bcadfb06fbf9
Haj B <phajib@gmail.com>
2020-04-07 10:32:05 -0700

    fixed fetch

--- src/Components/User/UserEdit.js
+++ src/Components/User/UserEdit.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-import { updateUser } from '../../actions/usersActions'
+import { userUpdate } from '../../actions/usersActions'
 
 import { Button, Form, Grid, Header, Segment, Icon } from 'semantic-ui-react'
-// import Swal from 'sweetalert2'
+import Swal from 'sweetalert2'
 
 class UserEdit extends Component {
     constructor(){
@@ -34,37 +34,37 @@ class UserEdit extends Component {
     }
 
     updateUser = (event) => {
+        console.log(this.state)
         event.preventDefault();
-        // console.log(this.state)
-        // let {username, password, about, picture_profile} = this.state
-        this.updateUser(this.state)
+        let {username, password, about, picture_profile} = this.state
+        // this.props.updateUser(this.state)
 
-        // fetch('http://localhost:3001/api/v1/user/edit', {
-        //     method: "PATCH",
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Accept':'application/json',
-        //         "Authorization" : `Bearer ${localStorage.getItem('jwt')}`
-        //     },
-        //     body: JSON.stringify({
-        //         username,
-        //         password,
-        //         about,
-        //         picture_profile
-        //     })
-        // })
-        // .then(resp => resp.json())
-        // .then(updatedProfile => {
-        //     updateUser(updatedProfile)
-        //     Swal.fire({
-        //         title: 'Profile Updated',
-        //         text: `Your profile has been updated!`,
-        //         icon: 'success',
-        //         confirmButtonText: 'Go to Profile',
-        //     }).then(function() {
-        //         window.history.go(-2);
-        //     })
-        // })
+        fetch('http://localhost:3001/api/v1/user/edit', {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept':'application/json',
+                "Authorization" : `Bearer ${localStorage.getItem('jwt')}`
+            },
+            body: JSON.stringify({
+                username,
+                password,
+                about,
+                picture_profile
+            })
+        })
+        .then(resp => resp.json())
+        .then(updatedProfile => {
+            this.props.userUpdate(updatedProfile)
+            Swal.fire({
+                title: 'Profile Updated',
+                text: `Your profile has been updated!`,
+                icon: 'success',
+                confirmButtonText: 'Go to Profile',
+            }).then(function() {
+                window.history.go(-1);
+            })
+        })
     }
 
     render() {
@@ -105,60 +105,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        updateUser: (updatedUser) => {dispatch(updateUser(updatedUser))}
+        userUpdate: (updatedUser) => {dispatch(userUpdate(updatedUser))}
     }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserEdit)
-
-
-
-        // return(
-        //     <div className="login-page-container">
-        //         <div id="edit-profile-form" className="login-signup-form">
-        //             <div className="edit-profile-header">
-        //                 <i className="fas fa-user-edit"></i>
-        //                 <h1>Edit Profile</h1>
-        //             </div>
-        //             <div className="field">
-        //                 <label className="label">Username</label>
-        //                 <div className="control">
-        //                     <input onChange={(event) => this.handleInputChange(event)}
-        //                     className="input" name="username" type="text" placeholder="Username" value={username}/>
-        //                 </div>
-        //             </div> 
-
-        //             <div className="field">
-        //                 <label className="label">Password</label>
-        //                 <div className="control">
-        //                     <input onChange={(event) => this.handleInputChange(event)}
-        //                     className="input" name="password" type="text" placeholder="Password"/>
-        //                 </div>
-        //             </div> 
-
-        //             <div className="field">
-        //                 <label className="label">about</label>
-        //                 <div className="control">
-        //                     <input onChange={(event) => this.handleInputChange(event)}
-        //                     className="input" name="about" type="text" placeholder="about" value={about}/>
-        //                 </div>
-        //             </div>
-
-        //             <div className="field">
-        //                 <label className="label">Profile Picture</label>
-        //                 <div className="control">
-        //                     <input onChange={(event) => this.handleInputChange(event)}
-        //                     className="input" name="profile_picture" type="file" placeholder="Profile Picture" value={profile_picture}/>
-        //                 </div>
-        //             </div>
-        //             <div className="control form-submit-container">
-        //                 <button onClick={(event) => this.updateUser(event, this.state)}
-        //                 className="button is-link">Update</button>
-        //                 {/* eslint-disable-next-line */}
-        //                 <span>  Or  <Link to="/profile">
-        //                  Back</Link></span>
-        //             </div>
-        //         </div>
-        //     </div>
-        // )
- 
