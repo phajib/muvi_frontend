82f929f1888ee585e6d170a41244ac8c53fa3155
Haj B <phajib@gmail.com>
2020-04-14 17:06:42 -0700

    layout adjustments

--- src/Components/User/UserMovies.js
+++ src/Components/User/UserMovies.js
@@ -1,27 +1,31 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-// eslint-disable-next-line
 import { userData } from '../../actions/usersActions'
+// eslint-disable-next-line
 import MovieCard from '../Movie/MovieCard'
+import MovieSampleCard from '../Movie/MovieSampleCard'
 
 // debugger
 class UserMovies extends Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.userData()
     }
 
     render() {
         console.log(this.props.userMovies)
         return (
-            <div className="container animated zoomIn">
-                <div className="container">
-                    <h2 className="text-success">Your Movie List</h2>
-                    {this.props.savedMovies.map(movie => {
-                        return <MovieCard key={movie.id} movie={movie} users={this.props.users}/>
+            <>
+                <h2 className="text-success">Your Movie List</h2>
+                <div className="row animated zoomIn">
+
+                    {this.props.userMovies.map(movie => {
+                        // return <MovieCard key={movie.id} movie={movie} users={this.props.users}/>
+                        return <MovieSampleCard key={movie.id} movie={movie} />
                     })}
+
                 </div>
-            </div>
+            </>
         )
     }
 }
@@ -34,31 +38,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        userData: () => {dispatch(userData())}
+        userData: () => { dispatch(userData()) }
     }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserMovies)
-// export default UserMovies
-
-
-
-
-// import React from 'react'
-// import MovieCard from '../Movie/MovieCard'
-
-// const UserMovies = (props) => {
-//     return (
-//       <div className="usermovies eight wide column animated zoomIn">
-//         <div className="usermovies">
-//             <h3 className="text-success">Your Movie List</h3>
-//             {props.userMovies.map(movie => {
-//                 return <MovieCard key={movie.id} movie={movie} />
-//             })
-//             }
-//         </div>
-//     </div>
-//     )
-// }
-
-// export default UserMovies
