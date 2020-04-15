e8355d8077d63fc7fe7f1b7034d5a9093e3388b8
Haj B <phajib@gmail.com>
2020-04-14 17:07:56 -0700

    layout adjustments

--- src/Components/User/UserPage.js
+++ src/Components/User/UserPage.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux'
 import UserDetails from "./UserDetails.js"
 import UserMovies from "../User/UserMovies"
 import UserComments from "./UserComments"
-
 // import { fetchUserPage } from '../../actions/usersActions' 
 
+// let HOST_URL = "http://localhost:3001/api/v1"
+// debugger
 class UserPage extends Component {
     constructor(){
         super()
@@ -18,10 +19,10 @@ class UserPage extends Component {
     }
 
     componentDidMount(){
-        // fetchUserPage()
-        // fetch(`http://localhost:3001/api/v1/user/${this.props.users.id}/info`)
+        // fetch(`${HOST_URL}/user/${this.props.userProfile}/info`)
         // .then(resp => resp.json())
         // .then( data => {
+        //     console.log(data)
         //     this.setState({
         //         user: data.user_info,
         //         savedMovies: data.savedMovies,
@@ -35,11 +36,15 @@ class UserPage extends Component {
             <div className="container">
                 <div className="row">
                     <UserDetails users={this.props.users} />
-                    <UserMovies users={this.props.users} savedMovies={this.state.savedMovies}/>
-                    {/* <UserMovies savedMovies={this.state.savedMovies} /> */}
-                    {/* <UserMovies userMovies={this.props.userMovies} /> */}
-                    <UserComments users={this.props.users}/>
-                    {/* <UserComments comments={this.state.comments} /> */}
+                    <div className="col">
+                        {/* <UserMovies users={this.props.users} savedMovies={this.props.savedMovies}/> */}
+                        {/* <UserMovies savedMovies={this.state.savedMovies} /> */}
+                        <UserMovies users={this.props.users} userMovies={this.props.userMovies} />
+                    </div>
+                    <div className="col">
+                        {/* <UserComments users={this.props.users} userComments={this.props.userComments} /> */}
+                    {/* <UserComments users={this.props.users}/> */}
+                    </div>
                 </div>
             </div>
         )
@@ -48,45 +53,10 @@ class UserPage extends Component {
 
 const mapStateToProps = state => {
     return {
-        users: state.users
+        users: state.users,
+        savedMovies: state.userMovies,
+        userComments: state.userComments
     }
 }
 
 export default connect(mapStateToProps, null)(UserPage)
-
-
-
-
-
-// import React, { Component } from 'react';
-// import UserContainer from './UserContainer'
-// import MymoviesContainer from './MymoviesContainer'
-// import CommentRating from '../components/CommentRating'
-// import Navbar from '../components/Navbar'
-// import '../style/profile.css'
-
-
-// class Profile extends Component {
-
-//   render() {
-//     let user;
-//     this.props.users.user ? user = this.props.users.user : user = this.props.users
-
-//     let myComments;
-//     user ? (myComments = this.props.userComments.filter(comm => comm.user_id === user.id)) : (myComments = [])
-
-//     return (
-//       <div className="Profile">
-//         <Navbar users={this.props.users} signOut={this.props.signOut}/>
-//         <div id="profileCon" className="ui grid">
-//           <UserDetails user={user}/>
-//           <UserMovies userMovies={this.props.userMovies} />
-//           <UserComments classes="mycomments four wide column" userComments={myComments} deleteComment={this.props.handleDeleteComment} mycomments={true} user={user} />
-//         </div>
-//       </div>
-//     )
-//   }
-
-// }
-
-// export default Profile
