ae905068113820942333d5c5a8fbf950e561af39
Haj B <phajib@gmail.com>
2020-04-23 01:32:17 -0700

    layout adjustments and utilising react moment

--- src/Components/User/UserDetails.js
+++ src/Components/User/UserDetails.js
@@ -4,21 +4,26 @@ import { Link } from 'react-router-dom'
 
 class UserDetails extends Component {
     render() {
-    let { username, about, profile_picture } = this.props.users.data.attributes
-    // let { username, about, profile_picture } = this.props.users.data
-    // let { username, about, profile_picture } = this.props.users
+
+    // let { username, about, profile_picture } = this.props.users.data.attributes
+    let { username, about, profile_picture } = this.props.userObj
+
     return (
-        <div className="container">
-            <div className="row animated fadeIn">
+        <div className="row bg-dark">
+            <div className="row mx-auto animated fadeIn">
                 <div className="col">
-                    <img id="profile_picture" className="rounded-circle img-thumbnail animated rubberBand" src={profile_picture} alt="profile_picture"></img>
-                    <div className="container">
-                        <Link className="btn btn-outline-success btn-sm" to="/profile/edit">Edit Profile</Link>
-                    </div>
+                    <img
+                        className="rounded-circle img-thumbnail mx-auto animated rubberBand"
+                        id="profile_picture"
+                        src={profile_picture}
+                        alt="profile_picture">
+                    </img>
+                    <br/><br/>
                 </div>
                 <div className="col animated flipInX" >
-                    <h2 id="username" className="text-success">{username}</h2>
-                    <p id="about" className="text-success">{about}</p>
+                    <h1 id="username" className="text-success">{username}</h1>
+                    <h3 id="about" className="text-success">{about}</h3>
+                    <Link className="btn btn-outline-success btn-sm" to="/profile/edit">Edit Profile</Link>
                 </div>
             </div>
         </div>
