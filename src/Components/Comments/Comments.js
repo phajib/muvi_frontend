3e2cac9e2c625b476cf7bd9b48b3971a9d0aac0a
Haj B <phajib@gmail.com>
2020-04-23 01:31:44 -0700

    layout adjustments and utilising react moment

--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import Moment from 'react-moment';
+import { Icon, Label } from 'semantic-ui-react'
 
 class Comments extends Component {
     render() {
-        let {username, content, created_at, user_id, id} = this.props.commentObj
+        let { movie_title, content, created_at, user_id, id, profile_picture, username } = this.props.commentObj
 
         return (
-            <div>
-                <div className="container">
-                    <h3 className="text-success">Your Comments</h3>
-                    <Link to={`/user/${user_id}`} className="text-success">{username}</Link>
-                    <p className="text-light">{content}</p>
-                    <p className="text-success">Created On: {created_at.split('T')[0]}</p> */}
-                    {this.props.users.data.id === user_id &&
-                        <button className="btn btn-light" onClick={() => this.props.deleteComment(id)}>
-                            Delete Comment
-                        </button>
-                    }
-                </div>
+            <div className="animated flipInX">
+                <Label image>
+                    {/* eslint-disable-next-line */}
+                    {this.props.users.data.id == user_id &&
+                        // <img src={this.props.users.data.attributes.profile_picture} alt='prof'/>}
+                        <img src={profile_picture} alt='prof'/>}
+                    {/* eslint-disable-next-line */}
+                    {this.props.users.data.id == user_id &&
+                        <Icon inverted color='green' size="small" name='close' onClick={() => this.props.deleteComment(id)} />}
+                    {username} - {movie_title} - <i>{content}  </i>
+                    <small><Moment format="DD-MM-YYYY">{created_at}</Moment></small>
+                </Label>
             </div>
         )
     }
 }
 
 const mapStateToProps = state => {
-    return { users: state.users }
+    return {
+        users: state.users
+    }
 }
 
 export default connect(mapStateToProps, null)(Comments)
