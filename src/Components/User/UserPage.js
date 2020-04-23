17b031372fc3fd43b4e3b16931ce3faf316ce2d1
Haj B <phajib@gmail.com>
2020-04-23 01:22:53 -0700

    utilising user, savedMovie, userComments

--- src/Components/User/UserPage.js
+++ src/Components/User/UserPage.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react"
+// eslint-disable-next-line
 import { connect } from 'react-redux'
 import UserDetails from "./UserDetails.js"
 import UserMovies from "../User/UserMovies"
 import UserComments from "./UserComments"
 // import { fetchUserPage } from '../../actions/usersActions' 
 
-// let HOST_URL = "http://localhost:3001/api/v1"
-// debugger
+let HOST_URL = "http://localhost:3001/api/v1"
+
 class UserPage extends Component {
     constructor(){
         super()
@@ -19,31 +20,34 @@ class UserPage extends Component {
     }
 
     componentDidMount(){
-        // fetch(`${HOST_URL}/user/${this.props.userProfile}/info`)
-        // .then(resp => resp.json())
-        // .then( data => {
-        //     console.log(data)
-        //     this.setState({
-        //         user: data.user_info,
-        //         savedMovies: data.savedMovies,
-        //         userComments: data.userComments
-        //     })
-        // })
+        fetch(`${HOST_URL}/user/${this.props.users.data.id}/info`)
+        .then(resp => resp.json())
+        .then( data => {
+            console.log(data)
+            this.setState({
+                user: data.user_info,
+                savedMovies: data.savedMovies,
+                userComments: data.userComments
+            })
+        })
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <UserDetails users={this.props.users} />
-                    <div className="col">
-                        {/* <UserMovies users={this.props.users} savedMovies={this.props.savedMovies}/> */}
-                        {/* <UserMovies savedMovies={this.state.savedMovies} /> */}
-                        <UserMovies users={this.props.users} userMovies={this.props.userMovies} />
+            <div className="container bg-dark">
+                    <div>
+                        {/* <UserDetails users={this.props.users} /> */}
+                        <UserDetails userObj={this.state.user} />
                     </div>
-                    <div className="col">
-                        {/* <UserComments users={this.props.users} userComments={this.props.userComments} /> */}
-                    {/* <UserComments users={this.props.users}/> */}
+                <div className="row">
+                    <div className="col-sm-6">
+                        {/* <UserMovies users={this.props.users} userMovies={this.props.userMovies} /> */}
+                        <UserMovies savedMovies={this.state.savedMovies} />
+                        {/* <UserMovies /> */}
+                    </div>
+                    <div className="col-sm-6">
+                        <UserComments userComments={this.state.userComments} />
+                        {/* <UserComments /> */}
                     </div>
                 </div>
             </div>
@@ -51,12 +55,14 @@ class UserPage extends Component {
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        users: state.users,
-        savedMovies: state.userMovies,
-        userComments: state.userComments
-    }
-}
+// const mapStateToProps = state => {
+//     return {
+//         users: state.users,
+//         // savedMovies: state.userMovies,
+//         // userComments: state.comments
+//     }
+// }
 
-export default connect(mapStateToProps, null)(UserPage)
+// export default connect(mapStateToProps, null)(UserPage)
+
+export default UserPage
