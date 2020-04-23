a82ac497167e408d97ec71086669bbbf43c06e65
Haj B <phajib@gmail.com>
2020-04-23 01:29:57 -0700

    fluid issues

--- src/Components/LoginSignUp/SignUp.js
+++ src/Components/LoginSignUp/SignUp.js
@@ -13,26 +13,25 @@ class Signup extends Component {
             <div className='animated bounceIn'>
             <Grid textAlign='center' style={{ height: '78vh' }} verticalAlign='middle'>
                     <Grid.Column style={{ maxWidth: 500 }}>
-                    <Icon name='address card' size='big' color='green' fluid icon='address card' />
+                    <Icon name='address card' size='big' color='green' icon='address card' />
                         <Header as='h2' color='green' textAlign='center'>
                             Sign Up for a MUVI account
                         </Header>
                         <Form size='large'>
                             <Segment stacked>
-                                <Form.Input name="username" fluid icon='user' iconPosition='left' placeholder='Username' onChange={(event) => this.handleInputChange(event)} />
-                                <Form.Input name="password" fluid icon='lock' iconPosition='left' placeholder='Password' type='password' onChange={(event) => this.handleInputChange(event)} />
-                                <Form.Input name="about" fluid icon='book' iconPosition='left' placeholder='Something about you...' onChange={(event) => this.handleInputChange(event)} />
-                                <Form.Input name="profile_picture" fluid icon='file globe' iconPosition='left' placeholder='Profile Picture wwww...' type='url' onChange={(event) => this.handleInputChange(event)} />
+                                <Form.Input name="username" icon='user' iconPosition='left' placeholder='Username' onChange={(event) => this.handleInputChange(event)} />
+                                <Form.Input name="password" icon='lock' iconPosition='left' placeholder='Password' type='password' onChange={(event) => this.handleInputChange(event)} />
+                                <Form.Input name="about" icon='book' iconPosition='left' placeholder='Something about you...' onChange={(event) => this.handleInputChange(event)} />
+                                <Form.Input name="profile_picture" icon='picture' iconPosition='left' placeholder='Profile Picture wwww...' type='url' onChange={(event) => this.handleInputChange(event)} />
 
-                                <Button color='green' fluid size='large' onClick={(event) => { this.props.createUser(event, this.state) }}>
+                                <Button color='green' size='large' onClick={(event) => { this.props.createUser(event, this.state) }}>
                                     Sign Up
                                 </Button>
-                                
                             </Segment>
                         </Form>
                         <Message>
                             {/* eslint-disable-next-line */}
-                                I just remembered who i was... <Button onClick={() => this.props.switchForm()}>Login</Button>
+                            I just remembered who i was... <Button onClick={() => this.props.switchForm()}>Login</Button>
                         </Message>
                     </Grid.Column>
                 </Grid>
