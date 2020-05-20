0bb9919152f27878928ceed4943be6920201d9f5
Haj B <phajib@gmail.com>
2020-05-20 13:24:07 -0700

    TMDB configurations

---
+++ src/config.js
@@ -0,0 +1,27 @@
+// Configuration for TMDB
+// To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
+
+const API_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "";
+
+// 019e8f375549e0bbd4a4191862ebc88f
+
+// Images
+// An image URL looks like this example:
+// http://image.tmdb.org/t/p/w780/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg
+
+const HOST_URL = "http://localhost:3001/api/v1"
+
+const IMAGE_URL = "https://image.tmdb.org/t/p/";
+
+//Sizes: w300, w780, w1280, original
+const BACKDROP_SIZE = "original";
+
+// w92, w154, w185, w342, w500, w780, original
+const MOVIE_IMAGE = "w342";
+const MOVIE_THUMB = "w342";
+
+const IMDB_PATH = "https://www.imdb.com/title/";
+const YOUTUBE_PATH = "https://www.youtube.com/watch?v=";
+
+export { API_URL, API_KEY, HOST_URL, IMAGE_URL, BACKDROP_SIZE, MOVIE_IMAGE, MOVIE_THUMB, IMDB_PATH, YOUTUBE_PATH };
